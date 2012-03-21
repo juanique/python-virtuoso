732ce4aa813a7595f67db2467c87b51b53b4764b
Juan Enrique Munoz <juanique@greedo.(none)>
2012-03-21 20:16:34 -0300

    minor code refactoring

--- tests_data.py
+++ tests_data.py
@@ -6,25 +6,32 @@ from testconfig import ISQLWrapperTestManager
 from rdflib import Namespace, Literal
 
 
-class DataTestCae(unittest.TestCase):
+class DataTestCase(unittest.TestCase):
 
     def setUp(self):
         self.config = Config("local_test_config.ini",
             default_manager=ISQLWrapperTestManager())
 
         url = "http://%s:%s%s"
-        url %= (self.config.host, self.config.endpoint_port, self.config.endpoint_path)
+        url %= (self.config.host, self.config.endpoint_port,
+            self.config.endpoint_path)
 
         self.endpoint = Endpoint(url)
-        self.isql = ISQLWrapper(self.config.host, self.config.user, self.config.password)
+        self.isql = ISQLWrapper(self.config.host, self.config.user,
+            self.config.password)
         self.isql.execute_cmd("SPARQL CLEAR GRAPH <%s>" % self.config.graph)
 
+    def assertGraphHasNumberOfTriples(self, n):
+        sparql_query = "SELECT * FROM <%s> WHERE {?s ?p ?o}" % self.config.graph
+        results = self.endpoint.query(sparql_query)
+        self.assertEquals(results.total_rows, n)
+
+    def assertGraphIsEmpty(self):
+        self.assertGraphHasNumberOfTriples(0)
 
     def tearDown(self):
         self.config.save("local_test_config.ini")
         self.isql.execute_cmd("SPARQL CLEAR GRAPH <%s>" % self.config.graph)
-
-
 
     def test_delete_uri(self):
         "Inserts a triple into the triplestore"
@@ -32,8 +39,7 @@ class DataTestCae(unittest.TestCase):
         ns = Namespace("https://github.com/juanique/artichoke/ns#")
         graph = self.config.graph
 
-        results = self.endpoint.query("SELECT * FROM <%s> WHERE {?s ?p ?o}" % graph)
-        self.assertEquals(results.total_rows, 0)
+        self.assertGraphIsEmpty()
 
         self.isql.insert(graph,
             ns["subject"], ns["predicate"], ns["object"])
@@ -41,8 +47,7 @@ class DataTestCae(unittest.TestCase):
         self.isql.delete(graph,
             ns["subject"], ns["predicate"], ns["object"])
 
-        results = self.endpoint.query("SELECT * FROM <%s> WHERE {?s ?p ?o}" % graph)
-        self.assertEquals(results.total_rows, 0)
+        self.assertGraphIsEmpty()
 
     def test_delete_literal(self):
         "Inserts a triple into the triplestore"
@@ -50,8 +55,7 @@ class DataTestCae(unittest.TestCase):
         ns = Namespace("https://github.com/juanique/artichoke/ns#")
         graph = self.config.graph
 
-        results = self.endpoint.query("SELECT * FROM <%s> WHERE {?s ?p ?o}" % graph)
-        self.assertEquals(results.total_rows, 0)
+        self.assertGraphIsEmpty()
 
         self.isql.insert(graph,
             ns["subject"], ns["predicate"], Literal("Hello"))
@@ -59,9 +63,7 @@ class DataTestCae(unittest.TestCase):
         self.isql.delete(graph,
             ns["subject"], ns["predicate"], Literal("Hello"))
 
-        results = self.endpoint.query("SELECT * FROM <%s> WHERE {?s ?p ?o}" % graph)
-        self.assertEquals(results.total_rows, 0)
-
+        self.assertGraphIsEmpty()
 
     def test_insert(self):
         "Inserts a triple into the triplestore"
@@ -71,9 +73,11 @@ class DataTestCae(unittest.TestCase):
 
         self.isql.insert(graph,
             ns["subject"], ns["predicate"], ns["object"])
-        results = self.endpoint.query("SELECT * FROM <%s> WHERE {?s ?p ?o}" % graph)
 
-        self.assertEquals(results.total_rows, 1)
+        query = "SELECT * FROM <%s> WHERE {?s ?p ?o}" % graph
+        results = self.endpoint.query(query)
+
+        self.assertGraphHasNumberOfTriples(1)
         self.assertEquals(results[0]['s'].value, str(ns["subject"]))
         self.assertEquals(results[0]['p'].value, str(ns["predicate"]))
         self.assertEquals(results[0]['o'].value, str(ns["object"]))
